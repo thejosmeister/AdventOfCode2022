f658e966aebfb4425faf372cf786325b2163de9a
thejosmeister <alphalanky@gmail.com>
2022-12-01 05:14:05 +0000

    day1 done

--- src/day1/day1part1.py
+++ src/day1/day1part1.py
@@ -1,1 +1,19 @@
 # Day 1 part 1
+from common.common import parse_input_into_list_of_strings
+
+list = parse_input_into_list_of_strings("input.txt")
+
+elf_totals = []
+
+current_tot = 0
+
+for l in list:
+    if l == "":
+        elf_totals.append(current_tot)
+        current_tot = 0
+    else:
+        current_tot += int(l)
+
+elf_totals.append(current_tot)
+
+print(max(elf_totals))
--- src/day1/day1part2.py
+++ src/day1/day1part2.py
@@ -1,1 +1,21 @@
 # Day 1 part 2
+from common.common import parse_input_into_list_of_strings
+
+list = parse_input_into_list_of_strings("input.txt")
+
+elf_totals = []
+
+current_tot = 0
+
+for l in list:
+    if l == "":
+        elf_totals.append(current_tot)
+        current_tot = 0
+    else:
+        current_tot += int(l)
+
+elf_totals.append(current_tot)
+
+elf_totals = sorted(elf_totals)
+
+print(sum(elf_totals[-3:]))

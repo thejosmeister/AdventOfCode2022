db3f4410b268c919c79ec22269e486359f93f46b
thejosmeister <alphalanky@gmail.com>
2022-12-03 08:09:37 +0000

    day3 done

--- src/day3/day3part1.py
+++ src/day3/day3part1.py
@@ -1,1 +1,27 @@
 # Day 3 part 1
+# quite a nice input to work with as can make a lot of assumptions
+import string
+
+import math
+
+from common.common import parse_input_into_list_of_strings
+
+bags = parse_input_into_list_of_strings("input.txt")
+
+lower = list(string.ascii_lowercase)
+upper = list(string.ascii_uppercase)
+letters = lower + upper
+
+
+def find_common_letter_score(x):
+    first = set( list( x[:math.floor(len(x)/2)] ) )
+    second = set(list(x[math.floor(len(x)/2):]))
+
+    com = first.intersection(second)
+
+    return letters.index(list(com)[0]) + 1
+
+
+bag_scores = [find_common_letter_score(x) for x in bags]
+
+print(sum(bag_scores))
--- src/day3/day3part2.py
+++ src/day3/day3part2.py
@@ -1,1 +1,27 @@
 # Day 3 part 2
+# my soln to part 1 made this pretty quick
+import string
+
+from common.common import parse_input_into_list_of_strings
+
+bags = parse_input_into_list_of_strings("input.txt")
+
+lower = list(string.ascii_lowercase)
+upper = list(string.ascii_uppercase)
+letters = lower + upper
+
+
+def common_letter_score_for_triplet(x):
+    first = set(list(x[0]))
+    second = set(list(x[1]))
+    third = set(list(x[2]))
+
+    com = first.intersection(second)
+    com2 = com.intersection(third)
+
+    return letters.index(list(com2)[0]) + 1
+
+
+bag_scores = [common_letter_score_for_triplet(bags[i:i + 3]) for i in range(0, len(bags), 3)]
+
+print(sum(bag_scores))

b23295e08685b16c10a7e4118e230ab60706a0a1
thejosmeister <alphalanky@gmail.com>
2022-12-09 19:29:10 +0000

    day 8

--- src/day8/day8part1.py
+++ src/day8/day8part1.py
@@ -1,1 +1,48 @@
 # Day 8 part 1
+# Was pretty fun, took me a while to work out where i was meant to reset the tallest tree
+from common.common import parse_input_into_list_of_strings
+
+rows = parse_input_into_list_of_strings("input.txt")
+
+a = [[*x] for x in rows ]
+grid = [ [int(z) for z in y] for y in a ]
+
+v_trees = []
+
+
+for i in range(len(grid[0])):
+    tallest = -1
+    for j in range(len(grid)):
+        if grid[j][i] > tallest:
+            if f"{i},{j}" not in v_trees:
+                v_trees.append(f"{i},{j}")
+            tallest = grid[j][i]
+
+
+for j in range(len(grid)):
+    tallest = -1
+    for i in range(len(grid[0])):
+        if grid[j][i] > tallest:
+            if f"{i},{j}" not in v_trees:
+                v_trees.append(f"{i},{j}")
+            tallest = grid[j][i]
+
+
+for i in range(len(grid[0])):
+    tallest = -1
+    for j in range(len(grid)-1, -1, -1):
+        if grid[j][i] > tallest:
+            if f"{i},{j}" not in v_trees:
+                v_trees.append(f"{i},{j}")
+            tallest = grid[j][i]
+
+
+for j in range(len(grid)):
+    tallest = -1
+    for i in range(len(grid[0])-1, -1, -1):
+        if grid[j][i] > tallest:
+            if f"{i},{j}" not in v_trees:
+                v_trees.append(f"{i},{j}")
+            tallest = grid[j][i]
+
+print(len(v_trees))
--- src/day8/day8part2.py
+++ src/day8/day8part2.py
@@ -1,1 +1,47 @@
 # Day 8 part 2
+# Cant read the question for this one
+from common.common import parse_input_into_list_of_strings
+
+rows = parse_input_into_list_of_strings("input.txt")
+
+a = [[*x] for x in rows ]
+grid = [ [int(z) for z in y] for y in a ]
+
+v_trees = []
+
+
+def calc_max_view_dist(i, j, param):
+    global grid
+
+    l_score = 0
+    for a in range(i - 1, -1, -1):
+        l_score += 1
+        if grid[j][a] >= param:
+            break
+
+    r_score = 0
+    for a in range(i + 1, len(grid[0])):
+        r_score += 1
+        if grid[j][a] >= param:
+            break
+
+    u_score = 0
+    for a in range(j - 1, -1, -1):
+        u_score += 1
+        if grid[a][i] >= param:
+            break
+
+    d_score = 0
+    for a in range(j + 1, len(grid)):
+        d_score += 1
+        if grid[a][i] >= param:
+            break
+
+    return l_score * r_score * u_score * d_score
+
+
+for i in range(1, len(grid[0]) - 1):
+    for j in range(1, len(grid) - 1):
+        v_trees.append(calc_max_view_dist(i, j, grid[j][i]))
+
+print(max(v_trees))

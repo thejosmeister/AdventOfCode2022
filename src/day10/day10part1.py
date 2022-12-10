a9771a5931e5bfaf2a0c6b25533a98276d4196c2
thejosmeister <alphalanky@gmail.com>
2022-12-10 10:09:08 +0000

    day 10

--- src/day10/day10part1.py
+++ src/day10/day10part1.py
@@ -1,1 +1,40 @@
 # Day 10 part 1
+# Clunky soln but pretty easy
+from common.common import parse_input_into_list_of_strings
+
+rows = parse_input_into_list_of_strings("input.txt")
+
+
+def parse_instr(x):
+    if x == 'noop':
+        return 0
+    return int(x.split(' ')[1])
+
+
+instr = [parse_instr(x) for x in rows]
+
+value = 1
+inst_index = 0
+cycle = False
+strengths = []
+for i in range(1,221):
+    if i % 40 == 20:
+        strengths.append(i * value)
+    if instr[inst_index] == 0:
+        inst_index += 1
+        continue
+
+    if cycle is True:
+        value += instr[inst_index]
+        inst_index += 1
+        cycle = False
+        continue
+
+    cycle = True
+
+
+print(sum(strengths))
+
+
+
+
--- src/day10/day10part2.py
+++ src/day10/day10part2.py
@@ -1,1 +1,52 @@
 # Day 10 part 2
+# Kind of fun, plenty of modulo
+from common.common import parse_input_into_list_of_strings
+
+rows = parse_input_into_list_of_strings("input.txt")
+
+
+def parse_instr(x):
+    if x == 'noop':
+        return 0
+    return int(x.split(' ')[1])
+
+
+instr = [parse_instr(x) for x in rows]
+
+value = 1
+inst_index = 0
+cycle = False
+lines = [[] for i in range(6)]
+line_no = 0
+
+for i in range(0, 240):
+    print(i, line_no)
+    if i > 0 and i % 40 == 0:
+        line_no += 1
+
+    if value + 1 >= i % 40 >= value - 1:
+        lines[line_no].append('#')
+    else:
+        lines[line_no].append('.')
+
+    if instr[inst_index] == 0:
+        inst_index += 1
+        continue
+
+    if cycle is True:
+        value += instr[inst_index]
+        inst_index += 1
+        cycle = False
+        continue
+
+    cycle = True
+
+for i in range(6):
+    [print(x, end='') for x in lines[i]]
+    print()
+
+
+
+
+
+
